e28040348ab3eaf13b8e6cf1ab29a901794fb880
BO YANG <boyang1020@gmail.com>
2018-02-13 13:32:58 -0800

    Revert "comment added"
    This reverts commit 4551d5ba7cb2fddd46d0c22592bd5094a01b62b4.

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-
-// my collection of 5 quotes with source, citation, year, and tags. 
 var quotes = [
     {
         quote: 'For myself, I am an optimist— it does not seem to be much use being anything else.',
@@ -42,7 +40,6 @@ var quotes = [
     }
 ];
 
-// random color generator displays randoms colors when another quote is displayed
 function random_bg_color() {
     var x = Math.floor(Math.random() * 256);
     var y = Math.floor(Math.random() * 256);
@@ -53,13 +50,13 @@ function random_bg_color() {
     document.body.style.background = bgColor;
 }
 
-// random qupte generator 
+
 function getRandomQuote() {
 
     return quotes[Math.floor(Math.random() * 5)];
 }
 
-// this prints random quote to the page with differnt properties
+
 function printQuote() {
     var quoteBox = document.getElementById('quote-box');
     var newQuote = getRandomQuote();
@@ -86,5 +83,5 @@ function printQuote() {
     random_bg_color();
 };
 
-// the quotes page will refresh every 30 seconds
+
 setInterval(printQuote, 30000);
