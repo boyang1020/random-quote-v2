4c0f58edb927438e2c167ec978a09e3d66a0345f
BO YANG <boyang1020@gmail.com>
2018-02-12 20:39:54 -0800

    treehouse-project-1
    In this project, you'll create an app that displays random famous
    quotes each time a button is clicked. You can display a quote from a
    famous athlete, politician, or historical figure:
    
    "The only thing we have to fear is fear itself." — Franklin Delano
    Roosevelt.
    
    You'll need to use your knowledge of basic JavaScript syntax, including
    variables, loops and object literals, to complete this project.
    
    To help you get started, we'll give you basic HTML and CSS, as well a
    JavaScript file containing some starter code. But you'll have to find
    the quotes yourself and build the data structure to store them in.
    
    This project is a fun and effective way for you to practice fundamental
    JavaScript skills. It also gives you a simple interactive portfolio
    piece to show off your understanding of JavaScript.

---
+++ js/script.js
@@ -0,0 +1,87 @@
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+var quotes = [
+    {
+        quote: 'For myself, I am an optimist— it does not seem to be much use being anything else.',
+        source: 'Winston Churchill',
+        citation: 'speech at the Lord Mayor\'s banquet, London',
+        year: '1954,',
+        tags: ' Optimisim'
+    },
+    {
+        quote: 'The reason we all like to think so well of others is that we are all afraid for ourselves. The basis of optimism is sheer terror.',
+        source: 'Oscar Wilde',
+        citation: 'The Picture of Dorian Gray',
+        year: '1891,',
+        tags: ' Optimisim'
+    },
+    {
+        quote: 'Ah, music. A magic beyond all we do here!',
+        source: 'J. K. Rowling',
+        citation: 'Harry Potter and the Sorcerer\'s Stone',
+        year: '1997,',
+        tags: ' Music'
+    },
+    {
+        quote: 'Aim above morality. Be not simply good; be good for something.',
+        source: 'Henry David Thoreau',
+        citation: 'Walden',
+        year: '1854,',
+        tags: ' Moral'
+    },
+    {
+        quote: 'There must be a limit to the mistakes one person can make, and when I get to the end of them, then I\'ll be through with them. That\'s a very comforting thought.',
+        source: 'L. M. Montgomery',
+        citation: 'Anne of Green Gables',
+        year: '1908,',
+        tags: ' Mistake'
+    }
+];
+
+function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    console.log(bgColor);
+
+    document.body.style.background = bgColor;
+}
+
+
+function getRandomQuote() {
+
+    return quotes[Math.floor(Math.random() * 5)];
+}
+
+
+function printQuote() {
+    var quoteBox = document.getElementById('quote-box');
+    var newQuote = getRandomQuote();
+
+    if (newQuote.citation === undefined && newQuote.year === undefined && newQuote.tags === undefined) {
+        quoteBox.innerHTML = '<p class="quote">' + newQuote.quote + '</p>' + '<p class="source">' + newQuote.source + '</p>';
+
+    } else if (newQuote.citation !== undefined && newQuote.year === undefined && newQuote.tags === undefined) {
+
+        quoteBox.innerHTML = '<p class="quote">' + newQuote.quote + '</p>' + '<p class="source">' + newQuote.source + '<span class="citation">' + newQuote.citation + '</span>' + '</p>';
+    } else if (newQuote.citation !== undefined && newQuote.year !== undefined && newQuote.tags === undefined) {
+        quoteBox.innerHTML = '<p class="quote">' + newQuote.quote + '</p>' + '<p class="source">' + newQuote.source + '<span class="citation">' + newQuote.citation + '</span>' + '<span class="year">' + newQuote.year + '</span>' + '</p>';
+
+
+    } else if (newQuote.citation === undefined && newQuote.year !== undefined && newQuote.tags === undefined) {
+        quoteBox.innerHTML = '<p class="quote">' + newQuote.quote + '</p>' + '<p class="source">' + newQuote.source + '<span class="year">' + newQuote.year + '</span>' + '</p>';
+
+    } else {
+
+        quoteBox.innerHTML = '<p class="quote">' + newQuote.quote + '</p>' + '<p class="source">' + newQuote.source + '<span class="citation">' + newQuote.citation + '</span>' + '<span class="year">' + newQuote.year + '</span>' + '<span class="tags">' + newQuote.tags + '</span>' + '</p>';
+    };
+
+
+    random_bg_color();
+};
+
+
+setInterval(printQuote, 30000);
